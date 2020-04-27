b0ea80aac262d722beb8967e2b392797c6ec232b
Khalil Stemmler <metroidman12@gmail.com>
2020-04-27 12:59:59 -0400

    Removed link from local state example config.

--- apollo-local-state/src/index.tsx
+++ apollo-local-state/src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
-import { ApolloClient, ApolloLink } from '@apollo/client';
+import { ApolloClient } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client';
 import 'todomvc-app-css/index.css'
 import { cache } from './cache';
 
 export const client = new ApolloClient({
   cache,
-  link: ApolloLink.empty(),
   connectToDevTools: true,
 });
 
