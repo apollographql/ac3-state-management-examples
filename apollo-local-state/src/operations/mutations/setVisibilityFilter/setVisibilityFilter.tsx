cab5a9d2cadd07898c27db09f5bff76784fa7eff
Nuwan W <nuwanwre@live.com>
2020-08-26 16:44:15 +0900

    fix: fixed a typo where visibilityFilter was imported as visiblityFilter in components

--- apollo-local-state/src/operations/mutations/setVisibilityFilter/setVisibilityFilter.tsx
+++ apollo-local-state/src/operations/mutations/setVisibilityFilter/setVisibilityFilter.tsx
@@ -1,9 +1,9 @@
 
-import { VisiblityFilter } from "../../../models/VisibilityFilter";
+import { VisibilityFilter } from "../../../models/VisibilityFilter";
 import { ReactiveVar } from "@apollo/client";
 
-export default (visibilityFilterVar: ReactiveVar<VisiblityFilter>) => {
-  return (filter: VisiblityFilter) => {
+export default (visibilityFilterVar: ReactiveVar<VisibilityFilter>) => {
+  return (filter: VisibilityFilter) => {
     visibilityFilterVar(filter);
   }
 }
